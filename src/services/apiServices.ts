b523fe7cad21f3a283063aa5c18b863878a29f47
dhini <ramdhininovita0811@gmail.com>
2025-08-22 11:24:36 +0700

    https ubah

--- src/services/apiServices.ts
+++ src/services/apiServices.ts
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_REACT_API_URL;
-export const STORAGE_URL = import.meta.env.VITE_REACT_API_STORAGE_URL;
 
 const apiClient = axios.create({
-  baseURL: BASE_URL,
-});
+    baseURL : BASE_URL,
+})
 
 export const isAxiosError = axios.isAxiosError;
 
